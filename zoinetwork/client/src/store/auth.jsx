bf25516ce251e9c06ca7f0b7e1147ed2b9bedf9b
Muhammad Nauman <nauman33183@gmail.com>
2024-05-30 14:34:43 +0500

    Update auth.jsx

--- zoinetwork/client/src/store/auth.jsx
+++ zoinetwork/client/src/store/auth.jsx
@@ -25,7 +25,7 @@ const LogoutUser = () => {
 const userAuthentication = async () => {
 
     try {
-        const response = await fetch("http://localhost:5000/api/auth/user", {
+        const response = await fetch("https://zoinetwork-api.vercel.app/api/auth/user", {
             method: "GET",
             headers:{
                 Authorization: authorizationToken
