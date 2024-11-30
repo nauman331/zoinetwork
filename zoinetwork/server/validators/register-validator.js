8a00a40358f5e74946f74507fe5b682ff755e326
Muhammad Nauman <nauman33183@gmail.com>
2024-11-30 19:58:12 +0500

    Update register-validator.js

--- zoinetwork/server/validators/register-validator.js
+++ zoinetwork/server/validators/register-validator.js
@@ -6,7 +6,7 @@ const signupSchema = z.object({
         .email('Invalid Email')
         .refine((val) => /@/.test(val), 'Not a valid email address'),
     password: z.string({ required_error: "Password is required" }).trim().min(6, 'Password must be atleast 6 characters long').max(50, 'Password cannot be more than 50 character long'),
-    phone: z.string({ required_error: "Phone number is required" }).trim().min(10, "Phone must be atleast 10 characters long").max(25, "Password cannot be more than 25 character long"),
+    phone: z.string({ required_error: "Phone number is required" }).trim().min(10, "Phone must be atleast 10 characters long").max(25, "Phone cannot be more than 25 character long"),
 });
 
 module.exports = signupSchema;
